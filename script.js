f91f37233027544fd9826ee6839510f648b64f95
Jefferson Santos Araújo <71133431+jefersonar-cmd@users.noreply.github.com>
2023-05-02 23:57:09 -0300

    atualizando arquivos

--- script.js
+++ script.js
@@ -2,55 +2,62 @@ const msgIn = document.getElementById('mensagem')
 const msgFi = document.getElementById('msgFi')
 const img = document.getElementById('imgs')
 
-function iniMsg(){
+function iniMsg(contador){
   msgIn.innerHTML = "Eu Te Amo Muito Nath S2</br>Você é a melhor coisa que me aconteceu!";
   img.src = "images/" + contador +".jpeg"
-  msgIn.style.opacity = 1;
-  img.style.opacity = 1;
+  msgIn.classList.add('show');
+  img.classList.add('show');
+  msgFi.classList.remove('show');
 }
 
-function fiMsg(){
+function fiMsg(contador){
   msgIn.innerHTML = "Eu Te Amo Muito Nath S2</br>Você é a melhor coisa que me aconteceu!";
   img.src = "images/" + contador +".jpeg"
-  msgFi.innerHTML = "Você é uma mulher Incrível!"
-  msgIn.style.opacity = 1;
-  img.style.opacity = 1;
-  msgFi.style.opacity = 1
+  msgFi.innerHTML = "Você é uma mulher Incrível!<br/>Minha Incrível Mulher, Princesa, Rainha e Meu Tudo!"
+  msgIn.classList.add('show');
+  img.classList.add('show');
+  msgFi.classList.add('show');
 }
 
-let contador = 0;
-    let intervalo = setInterval(function() {
+function startInterval() {
+  let contador = 0;
+  let intervalo = setInterval(function() {
     if (contador < 5) {
       console.log(contador)
       contador++;
-        if(contador == 5){
-            fiMsg()
-            setTimeout(function() {
-              msgIn.style.opacity = 0;
-              img.style.opacity = 0;
-              msgFi.style.opacity = 0
-            }, 10000);
-            contador = 0
-        }else{
-          iniMsg()
-          setTimeout(function() {
-            msgIn.style.opacity = 0;
-            img.style.opacity = 0;
-          }, 1500);
-        }
+      if (contador >= 5) {
+        fiMsg(contador - 1)
+        setTimeout(function() {
+          img.classList.remove('show');
+          msgFi.classList.remove('show');
+        }, 10000);
+        clearInterval(intervalo);
+        setTimeout(function() {
+          startInterval();
+          console.log("Intervalo reiniciado.");
+        }, 15000);
+      } else {
+        iniMsg(contador - 1)
+        setTimeout(function() {
+          img.classList.remove('show');
+        }, 2500);
+      }
     } else {
-        //clearInterval(intervalo);
-        contador = 0
+      contador = 0
     }
-}, 3000);
-// Obtém o elemento de áudio
+  }, 5000);
+  return intervalo;
+}
+
+// Inicia o intervalo
+let intervalo = startInterval();
+
+// Obtém o elemento de áudio e define o volume para 50%
 var audio = document.getElementById("myAudio");
-document.addEventListener("load", function() {
-  audio.play();
-});
+audio.volume = 0.1;
+
+// Repete a música quando terminar
 audio.addEventListener('ended', function() {
   this.currentTime = 0;
   this.play();
 }, false);
-
-
