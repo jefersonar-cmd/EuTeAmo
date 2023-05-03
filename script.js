a78c4e42468f8a45ad0f815369219e587a8f3772
Jefferson Santos Araújo <71133431+jefersonar-cmd@users.noreply.github.com>
2023-05-03 01:48:05 -0300

    refeito quase todo o código

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const msgIn = document.getElementById('mensagem')
 const msgFi = document.getElementById('msgFi')
 const img = document.getElementById('imgs')
-var audio = document.getElementById("myAudio");
+const audio = document.getElementById("myAudio");
+const playButton = document.getElementById('play');
+const btn = document.getElementById('btn')
 
-audio.play();
+document.addEventListener('load', function() {
+  audio.pause();
+  msgFi.style.display = 'none';
+  img.style.display = 'none';
+  msgFi.style.display = 'none';
+})
+
+playButton.addEventListener('click', function () {
+  playAudio();
+  playButton.style.display = 'none';
+  btn.style.display = 'none';
+  startInterval();
+});
+
+function playAudio() {
+  return new Promise((resolve, reject) => {
+    audio.volume = 0.1;
+    audio.play();
+    audio.onended = resolve;
+    audio.onerror = reject;
+  });
+}
 
 function iniMsg(contador){
   msgIn.innerHTML = "Eu Te Amo Muito Nath S2</br>Você é a melhor coisa que me aconteceu!";
@@ -23,11 +46,10 @@ function fiMsg(contador){
 }
 
 function startInterval() {
-  //audio.play();
   let contador = 0;
   let intervalo = setInterval(function() {
     if (contador < 5) {
-      //console.log(contador)
+      console.log(contador)
       contador++;
       if (contador >= 5) {
         fiMsg(contador - 1)
@@ -54,7 +76,7 @@ function startInterval() {
 }
 
 // Inicia o intervalo
-let intervalo = startInterval();
+
 
 // Obtém o elemento de áudio e define o volume para 50%
 audio.volume = 0.1;
